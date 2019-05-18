f70a12a791aab67a86948efa602d83d80ae9bedf
blitz-003 <elmanpathan@gmail.com>
2019-05-19 01:49:31 +0800

    lab8 done

--- lab8/functions.inc.php
+++ lab8/functions.inc.php
@@ -3,6 +3,15 @@
     function outputOrderRow($file, $title, $quantity, $price) {
         echo "<tr>";
         //TODO
+
+        echo "<td><img src=\"images/books/tinysquare/$file\"></td>";
+        echo "<td class=\"mdl-data-table__cell--non-numeric\">$title</td>";
+        echo "<td>$quantity</td>" ;
+        echo "<td>$$price</td>" ;
+        $total = $quantity * $price ;
+        echo "<td>$$total</td>";
+
         echo "</tr>";
+        
     }
 ?>
